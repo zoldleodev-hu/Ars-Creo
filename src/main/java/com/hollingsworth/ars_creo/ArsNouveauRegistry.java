419035ec95c630a92b72c8a10e0d38c6bab8c7b7
Bailey Hollingsworth <baileyholl@gmail.com>
2025-03-08 09:30:06 -0600

    add docs

--- src/main/java/com/hollingsworth/ars_creo/ArsNouveauRegistry.java
+++ src/main/java/com/hollingsworth/ars_creo/ArsNouveauRegistry.java
@@ -4,11 +4,19 @@ import com.hollingsworth.ars_creo.common.registry.ModBlockRegistry;
 import com.hollingsworth.arsnouveau.api.documentation.ReloadDocumentationEvent;
 import com.hollingsworth.arsnouveau.api.documentation.builder.DocEntryBuilder;
 import com.hollingsworth.arsnouveau.api.registry.DocumentationRegistry;
+import com.simibubi.create.AllBlocks;
 
 public class ArsNouveauRegistry {
     public static void registerDocumentation(ReloadDocumentationEvent event){
         DocumentationRegistry.registerEntry(DocumentationRegistry.CRAFTING, new DocEntryBuilder(ArsCreo.MODID, DocumentationRegistry.CRAFTING, ModBlockRegistry.STARBY_WHEEL.get().asItem())
                 .withIntroPage()
                 .withCraftingPages(ModBlockRegistry.STARBY_WHEEL.get()).build());
+        DocumentationRegistry.registerEntry(DocumentationRegistry.CRAFTING, new DocEntryBuilder(ArsCreo.MODID, DocumentationRegistry.CRAFTING, AllBlocks.DISPLAY_LINK.get().asItem()).withName("ars_creo.display_link")
+                .withIntroPage()
+                .withCraftingPages(AllBlocks.DISPLAY_LINK, AllBlocks.DISPLAY_BOARD).build());
+        DocumentationRegistry.registerEntry(DocumentationRegistry.CRAFTING, new DocEntryBuilder(ArsCreo.MODID, DocumentationRegistry.CRAFTING, AllBlocks.FLUID_TANK.get().asItem())
+                        .withName("ars_creo.fluid_tank")
+                .withIntroPage().build());
+
     }
 }
