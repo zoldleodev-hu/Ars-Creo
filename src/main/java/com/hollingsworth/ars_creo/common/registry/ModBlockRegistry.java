59ef88833383ced72ef1b04436d8b635641641e4
zoldleodev-hu <zoldleo.dev@gmail.com>
2026-05-17 18:35:31 +0200

    Optimized starbuncle wheel, fixed starbuncle wheel item rendering (#44)
    * Optimized starbuncle wheel, fixed starbuncle wheel item rendering
    
    * small improvement to starbuncle wheels
    
    ---------
    
    Co-authored-by: ZoldLeo <zoldleogameplay@gmail.com>

--- src/main/java/com/hollingsworth/ars_creo/common/registry/ModBlockRegistry.java
+++ src/main/java/com/hollingsworth/ars_creo/common/registry/ModBlockRegistry.java
@@ -1,13 +1,11 @@
 package com.hollingsworth.ars_creo.common.registry;
 
 import com.hollingsworth.ars_creo.ArsCreo;
-import com.hollingsworth.ars_creo.client.render.StarbuncleWheelRenderer;
 import com.hollingsworth.ars_creo.common.block.StarbuncleWheelBlock;
 import com.hollingsworth.ars_creo.common.block.StarbuncleWheelTile;
 import com.hollingsworth.ars_creo.common.lib.LibBlock;
-import com.hollingsworth.arsnouveau.common.items.RendererBlockItem;
+import com.hollingsworth.arsnouveau.common.items.AnimBlockItem;
 import com.hollingsworth.arsnouveau.setup.registry.ItemsRegistry;
-import net.minecraft.client.renderer.BlockEntityWithoutLevelRenderer;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.world.item.Item;
@@ -17,27 +15,18 @@ import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.neoforged.neoforge.registries.DeferredHolder;
 import net.neoforged.neoforge.registries.DeferredRegister;
 
-import java.util.function.Supplier;
-
-
-
 public class ModBlockRegistry {
     public static final DeferredRegister<Block> BLOCK_REG = DeferredRegister.create(BuiltInRegistries.BLOCK, ArsCreo.MODID);
     public static final DeferredRegister<BlockEntityType<?>> BLOCK_ENTITY_REG = DeferredRegister.create(Registries.BLOCK_ENTITY_TYPE, ArsCreo.MODID);
 
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(BuiltInRegistries.ITEM, ArsCreo.MODID);
     public static DeferredHolder<Block, StarbuncleWheelBlock> STARBY_WHEEL = BLOCK_REG.register(LibBlock.STARBUNCLE_WHEEL, () -> new StarbuncleWheelBlock(defaultProperties().noOcclusion().lightLevel((state) -> 10)));
+    @SuppressWarnings("all")
     public static DeferredHolder<BlockEntityType<?>, BlockEntityType<StarbuncleWheelTile>> STARBY_TILE = BLOCK_ENTITY_REG.register(LibBlock.STARBUNCLE_WHEEL, () -> BlockEntityType.Builder.of(StarbuncleWheelTile::new, STARBY_WHEEL.get()).build(null));
     public static DeferredHolder<Item, Item> STARBY_WHEEL_ITEM;
 
     public static void onBlockItemsRegistry() {
-        STARBY_WHEEL_ITEM = ITEMS.register(LibBlock.STARBUNCLE_WHEEL, () -> new RendererBlockItem(ModBlockRegistry.STARBY_WHEEL.get(), ItemsRegistry.defaultItemProperties()) {
-            @Override
-            public Supplier<BlockEntityWithoutLevelRenderer> getRenderer() {
-                return StarbuncleWheelRenderer::getISTER;
-            }
-        });
-
+        STARBY_WHEEL_ITEM = ITEMS.register(LibBlock.STARBUNCLE_WHEEL, () -> new AnimBlockItem(ModBlockRegistry.STARBY_WHEEL.get(), ItemsRegistry.defaultItemProperties()));
     }
 
     public static Block.Properties defaultProperties(){
